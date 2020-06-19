5014c694f737a2bb665dace2d491b1ba8b25afbf
xiaoboya <xiaoboya@owntek.com>
2020-06-19 10:27:21 +0800

    add:添加session的key

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-06-13 21:49:06
- * @LastEditTime: 2020-06-19 10:23:02
+ * @LastEditTime: 2020-06-19 10:26:54
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /SelfDisk/main.go
@@ -13,7 +13,9 @@ import (
 	"SelfDisk/brain"
 	"SelfDisk/router"
 	"SelfDisk/utils"
+	"time"
 
+	"github.com/gin-gonic/contrib/sessions"
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
 )
@@ -23,6 +25,15 @@ func main() {
 	// 生成公私钥
 	utils.InitKeys()
 	var r = gin.Default()
+
+	// 设置session
+	store := sessions.NewCookieStore([]byte("secret"))
+	store.Options(sessions.Options{
+		MaxAge: int(10 * time.Minute), //10min
+		Path:   "/",
+	})
+	r.Use(sessions.Sessions("mysession", store))
+
 	router.MainURL(r)
 	r.Run(":8080")
 }
