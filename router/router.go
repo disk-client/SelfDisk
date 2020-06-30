07be86db29794e7c90e7ba282641b90ac1b21168
xiaoboya <xiaoboya@owntek.com>
2020-06-30 10:34:02 +0800

    add:添加方法（下载）

--- router/router.go
+++ router/router.go
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-06-14 22:18:04
- * @LastEditTime: 2020-06-18 16:38:43
+ * @LastEditTime: 2020-06-20 17:22:36
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /SelfDisk/router.go
@@ -20,10 +20,15 @@ func MainURL(r *gin.Engine) {
 	r.POST("/login", views.Login)
 	r.POST("/register", views.Register)
 
-	// userURL 用户相关路由
-	var clientURL = r.Group("/download")
+	// downloadURL 下载相关路由
+	var downloadURL = r.Group("/download")
 	{
-		clientURL.POST("/client", views.DownloadClient)
-		clientURL.POST("/server", views.DownloadServer)
+		downloadURL.POST("/client", views.DownloadClient)
+		downloadURL.POST("/server", views.DownloadServer)
+	}
+
+	var clientURL = r.Group("/client")
+	{
+		clientURL.POST("/server/ipaddr", views.DiskServerAddr)
 	}
 }
